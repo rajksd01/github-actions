4e86cca38a62a72744ce91c1a557e9a478d7f0ef
unknown <rajksd9@gmail.com>
2024-08-20 15:27:54 +0530

    Initial Commit for the github-actions assignment

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express= require('express');
+const { addTodo, getTodo, deleteTodo, listTodos } = require('./todoManager.js');
+
+const app = express();
+app.use(express.json());
+
+app.post('/todos', (req, res) => {
+  const { task } = req.body;
+  if (!task) {
+    return res.status(400).json({ error: 'Task is required' });
+  }
+  const todo = addTodo(task);
+  res.status(201).json(todo);
+});
+
+app.get('/todos/:id', (req, res) => {
+  const todo = getTodo(Number(req.params.id));
+  if (todo) {
+    res.json(todo);
+  } else {
+    res.status(404).json({ error: 'Todo not found' });
+  }
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const success = deleteTodo(Number(req.params.id));
+  if (success) {
+    res.status(204).end();
+  } else {
+    res.status(404).json({ error: 'Todo not found' });
+  }
+});
+
+app.get('/todos', (req, res) => {
+  res.json(listTodos());
+});
+
+
+
+module.exports= app;
