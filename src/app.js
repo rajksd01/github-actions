918eedbb076ab74186dd18a7d2e1f7b40729f197
unknown <rajksd9@gmail.com>
2024-08-20 17:17:42 +0530

    Test for notifications

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const { addTodo, getTodo, deleteTodo, listTodos } = require('./todoManager.js');
 
 const app = express();
 app.use(express.json());
-console.log("test")
+console.log("test1")
 
 app.post('/todos', (req, res) => {
   const { task } = req.body;
